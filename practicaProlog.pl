8d7edd76af9bfdc4008746c6ba0b8aca5b0997cd
Rafael Alejandro <rafa4apple@gmail.com>
2019-04-02 00:17:55 +0200

    ColoresTorre va bastante bien

--- practicaProlog.pl
+++ practicaProlog.pl
@@ -1,3 +1,9 @@
+%:-module(practica1_MelladoClimentRafael,_,[]).
+%About students.
+alumno_prode(mellado,climent,rafael,y16m032).
+alumno_prode(mellado,climent,rafael,y16m032).
+alumno_prode(vazquez,acevedo,jorge,y16m032).
+
 0.
 nat(0).
 
@@ -16,6 +22,13 @@ mayorOigual(s(A),s(B)):- mayorOigual(A,B).
 
 suma(0,X,X):-nat(X).
 suma(s(X),Y,s(Z)):-suma(X,Y,Z).
+
+%Se lee igual que la suma el primero menos el segundo es igual al tercero ?
+resta(A,0, A).				% a - 0 = a
+resta(A, s(B), C) :- 				% If a - b = (c + 1) then a - (b + 1) = c
+resta(A, B, s(C)).	
+
+
 
 color(r).
 color(v).
@@ -52,4 +65,13 @@ esTorre([X|[Y|Ys]]):-esPiezas_orden(X,Y),esTorre([Y|Ys]).
 %Lo primero que hace es verifica si la lista es una torre, y después mira si la altura es la pasada
 %o te la calcula, si es que lo dejas como Variable.
 
-%alturaTorre([X|Ys],A):-esTorre([X|Ys]),sumaAlturas(X,alturaTorre,A)
+alturaTorre([X],A):-sumaAlturas(X,pieza(0,0,0,a),A).
+%alturaTorre([X|[Y|Ys]],A):-sumaAlturas(X,Y,B),suma(B,alturaTorre()).
+
+
+%Para coloresTorre([],[]), utilizamos la función es miembro
+%Falla si le pasas un lista con más colores de los que tenga la torre.
+colorsTorre([pieza(_,_,_,Color1)],[C|Cs]):-es_miembro(Color1,[C|Cs]).
+colorsTorre([pieza(_,_,_,Color2)|Ys],[C|Cs]):-es_miembro(Color2,[C|Cs]),colorsTorre(Ys,Cs).
+
+coloresTorre(X,Y):-esTorre(X),colorsTorre(X,Y).
